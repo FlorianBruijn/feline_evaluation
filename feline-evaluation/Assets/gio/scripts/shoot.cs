9618d2435e5e802a51b4a6661824ba1efa4f2a07
orbitmiyuki <35809@ma-web.nl>
2023-06-15 10:41:55 +0200

    'gio

--- feline-evaluation/Assets/gio/scripts/shoot.cs
+++ feline-evaluation/Assets/gio/scripts/shoot.cs
@@ -14,5 +14,22 @@ public class shoot : MonoBehaviour
             Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
             instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, -speed));
         }
+        
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        handleHit(coll.gameObject);
+    }
+    private void OnTriggerEnter(Collider coll)
+    {
+        handleHit(coll.gameObject);
+    }
+    private void handleHit(GameObject other)
+    {
+        if (other.tag == "enemy")
+        {
+            Destroy(other, 2f);
+        }
     }
 }
