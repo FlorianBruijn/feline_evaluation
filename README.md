2fc62ae20df9222c9ca3f3cb6056a9e3160344ef
florian <34652@ma-web.nl>
2023-06-22 12:07:59 +0200

    ship movement

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ o.o4 : player kan bewegen moor nog niet goed
 0.06 : 2 mappen gemaakt
 
 pickup : pickup aangemaakt
+
+ship movement : fps movement in ship gemaakt
---
+++ feline-evaluation/Assets/florian/scripts/1st_person/fps_controler.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fps_controler : MonoBehaviour
+{
+    public float speed;
+
+    public float PlayerHeight;
+    public LayerMask WhatIsGround;
+    bool Grounded;
+    public float GroundDrag;
+
+    float hotizontalInput;
+    float verticalInput;
+
+    Vector3 moveDir;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody> ();
+        rb.freezeRotation = true;
+    }
+
+    private void MyInput()
+    {
+        hotizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDir = transform.forward * verticalInput + transform.right * hotizontalInput;
+
+        rb.AddForce(moveDir.normalized * speed * 10f, ForceMode.Force);
+
+        if (Grounded)
+            rb.drag = GroundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyInput();
+        Grounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight * 0.5f + 0.2f, WhatIsGround);
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+}
